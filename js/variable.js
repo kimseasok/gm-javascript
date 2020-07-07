e799b78b07b10b65c494081bf93259ee30a19191
Kimsea Sok <basicblogtalk@gmail.com>
2020-07-06 23:12:43 -0400

    Episode 2: Data Types and Variables

---
+++ js/variable.js
@@ -0,0 +1,38 @@
+var num = 0;
+
+var num1 = 3,
+
+num2 = 6,
+
+num4 = 8;
+
+
+console.log( num, num1, num2, num4 );
+
+var num5 = num2;
+
+console.log( num5 );
+
+var a = b = c = 0;
+
+console.log( a, b, c );
+
+b = 2;
+
+console.log( a, b, c );
+
+var message = 'Hello John! How are you?';
+
+console.log( message );
+
+var answer = "I'm fine. Thanks! And you?";
+
+console.log( answer );
+
+var answer1 = 'Hello "Jane", how are you?';
+
+console.log( answer1 );
+
+var A = 1;
+
+console.log( a, A );
