dda4ea7dae838badbfa6d63eacf877f66de011f6
Kimsea Sok <basicblogtalk@gmail.com>
2020-07-08 02:17:39 -0400

    Episode 3: Assignment and Maths Operators

---
+++ js/operators.js
@@ -0,0 +1,80 @@
+let pen = 12;
+let book = 15;
+
+let pens = pen * 2;
+
+let buy = pen + book;
+
+let expensive = book - pen;
+
+let divid = 19 / 5;
+
+let modulas = 5 % 4;
+
+let power = 5 ** 3;
+
+console.log( pens );
+
+console.log( buy );
+
+console.log ( expensive );
+
+console.log( divid );
+
+console.log( modulas );
+
+console.log( power );
+
+
+// Math assignment operators
+
+let add = 2;
+
+// add assignment operator
+
+add += 1;
+
+// add = add + 1;
+
+console.log( add );
+
+let divid_ass = 10;
+
+divid_ass /= 2;
+
+// divid_ass = divid_ass / 2;
+
+console.log( divid_ass );
+
+let multiply_ass = 10;
+
+multiply_ass *= 2;
+
+console.log( multiply_ass );
+
+let power_ass = 5;
+
+power_ass **= 2;
+
+console.log( power );
+
+
+let increase = 5;
+
+console.log( increase );
+
+++increase;
+
+increase++;
+
+console.log( increase );
+
+let decrease = 8;
+
+console.log( decrease );
+
+--decrease;
+
+decrease--;
+
+console.log( decrease );
